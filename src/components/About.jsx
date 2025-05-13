40f108da04faadf69ba04a603a10d455a6925d41
Rahul-Biradar-09 <rahulbiradar1111@gmail.com>
2025-05-13 19:43:14 +0530

    2.0 update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
             Hi, I'm Rahul Biradar
           </h1>
           <p className="text-sm sm:text-base md:text-lg lg:text-xl text-gray-700 leading-relaxed">
-            I'm a Full Stack Developer specializing in the <strong>MERN stack</strong>. I craft responsive, scalable, and user-friendly web applications with a strong focus on performance, clean code, and seamless user experiences. Passionate about building modern solutions that solve real-world problems through technology.
+            I'm a Full Stack Developer specializing in the <strong>MERN stack</strong>. I build responsive, scalable, and user-friendly web applications with a strong focus on performance, clean code, and seamless user experiences. Passionate about building modern solutions that solve real-world problems through technology.
           </p>
         </div>
       </div>
