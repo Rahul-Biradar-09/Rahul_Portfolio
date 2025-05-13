192a196999055d00e16e44a4a721ed279e31a16b
Rahul-Biradar-09 <rahulbiradar1111@gmail.com>
2025-05-13 19:52:32 +0530

    3.0 update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
       className="min-h-screen bg-cover bg-center bg-[url('https://images.pexels.com/photos/3184634/pexels-photo-3184634.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')]"
     >
       <div className="min-h-screen flex items-center justify-center px-4 sm:px-6 lg:px-8 py-20">
-        <div className="bg-white/80 backdrop-blur-lg p-6 sm:p-8 md:p-12 lg:p-14 rounded-3xl shadow-2xl max-w-xl sm:max-w-2xl lg:max-w-3xl w-full text-center text-gray-900">
+        <div className="bg-white/80 mt-14 backdrop-blur-lg p-6 sm:p-8 md:p-12 lg:p-14 rounded-3xl shadow-2xl max-w-xl sm:max-w-2xl lg:max-w-3xl w-full text-center text-gray-900">
           <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold mb-4 sm:mb-6">
             Hi, I'm Rahul Biradar
           </h1>
